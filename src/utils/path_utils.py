8aee398ac64d00f1b3b08d9a17083636fbbc9d5c
CamilaBermudez <cbermudezv@unal.edu.co>
2026-04-20 20:44:36 +0200

    A new function just to format the name was added

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -3,14 +3,19 @@ import re
 from pathlib import Path
 from typing import Tuple, Union
 
+
+def fixed_file_naming(base_name: str, padding: int = 2) -> str:
+    padded_name = re.sub(r'^\d+', lambda m: m.group().zfill(padding), base_name)
+    processed_name = padded_name.replace(" ", "").replace("-", "_").replace("f.", "f_")
+    return processed_name
+
 def format_filename(base_name: str, output_folder: Union[str, Path], padding: int = 2) -> Tuple[Path, str, str]:
     """
     Format a base filename for Kraken output
     Returns: (output_path, output_filename, processed_name)
     """
     output_folder = Path(output_folder)
-    padded_name = re.sub(r'^\d+', lambda m: m.group().zfill(padding), base_name)
-    processed_name = padded_name.replace(" ", "").replace("-", "_").replace("f.", "f_")
+    processed_name = fixed_file_naming(base_name, padding)
     output_filename = f"{processed_name}.json"
     output_path = os.path.join(output_folder, output_filename) 
     return output_path, output_filename, processed_name
